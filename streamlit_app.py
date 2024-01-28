64b68d660ebb59f159523337261adb179bdb3976
Saksham Dev <105414841+sakshamdev123@users.noreply.github.com>
2024-01-28 23:22:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,90 @@
-import altair as alt
 import numpy as np
 import pandas as pd
-import streamlit as st
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense,LSTM,Dropout
 
-"""
-# Welcome to Streamlit!
+data = pd.read_csv('Google_train_data.csv')
+data.head()
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+data.info()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+data["Close"]=pd.to_numeric(data.Close,errors='coerce')
+data = data.dropna()
+trainData = data.iloc[:,4:5].values
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+data.info()
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+sc = MinMaxScaler(feature_range=(0,1))
+trainData = sc.fit_transform(trainData)
+trainData.shape
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+X_train = []
+y_train = []
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+for i in range (60,1149):
+    X_train.append(trainData[i-60:i,0])
+    y_train.append(trainData[i,0])
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+X_train,y_train = np.array(X_train),np.array(y_train)
+
+X_train = np.reshape(X_train,(X_train.shape[0],X_train.shape[1],1))
+X_train.shape
+
+model = Sequential()
+
+model.add(LSTM(units=100, return_sequences = True, input_shape =(X_train.shape[1],1)))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=100, return_sequences = True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=100, return_sequences = True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=100, return_sequences = False))
+model.add(Dropout(0.2))
+
+model.add(Dense(units =1))
+model.compile(optimizer='adam',loss="mean_squared_error")
+
+hist = model.fit(X_train, y_train, epochs = 20, batch_size = 32, verbose=2)
+
+plt.plot(hist.history['loss'])
+plt.title('Training model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train'], loc='upper left')
+plt.show()
+
+testData = pd.read_csv('Google_test_data.csv')
+testData["Close"]=pd.to_numeric(testData.Close,errors='coerce')
+testData = testData.dropna()
+testData = testData.iloc[:,4:5]
+y_test = testData.iloc[60:,0:].values
+#input array for the model
+inputClosing = testData.iloc[:,0:].values
+inputClosing_scaled = sc.transform(inputClosing)
+inputClosing_scaled.shape
+X_test = []
+length = len(testData)
+timestep = 60
+for i in range(timestep,length):
+    X_test.append(inputClosing_scaled[i-timestep:i,0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
+X_test.shape
+
+y_pred = model.predict(X_test)
+y_pred
+
+predicted_price = sc.inverse_transform(y_pred)
+
+plt.plot(y_test, color = 'red', label = 'Actual Stock Price')
+plt.plot(predicted_price, color = 'green', label = 'Predicted Stock Price')
+plt.title('Google stock price prediction')
+plt.xlabel('Time')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
